21a4594b482d1569082ab6a8eff2affe53c469ec
Michal Predotka <mivoligo@gmail.com>
2024-12-19 19:20:17 +0000

    Add table view based on data_table_2 package

--- lib/table/views/table_page.dart
+++ lib/table/views/table_page.dart
@@ -1,4 +1,6 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'package:satagromini/table/data/elements.dart';
 
 class TablePage extends StatelessWidget {
   const TablePage({super.key});
@@ -8,6 +10,38 @@ class TablePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Table'),
+      ),
+      body: DataTable2(
+        columns: [
+          const DataColumn2(label: Text('Element'), size: ColumnSize.M),
+          ...elementsData.values.first.indexed.map(
+            (indexedList) => DataColumn2(
+              label: Text('${indexedList.$1 + 1}'),
+              numeric: true,
+              onSort: (columnIndex, ascending) {},
+            ),
+          )
+        ],
+        rows: elementsData.entries
+            .map(
+              (entry) => DataRow2(
+                cells: [
+                  DataCell(Text(entry.key)),
+                  ...entry.value.map(
+                    (doubleValue) => DataCell(Text(doubleValue.toString())),
+                  )
+                ],
+              ),
+            )
+            .toList(growable: false),
+        columnSpacing: 12,
+        fixedTopRows: 1,
+        fixedLeftColumns: 1,
+        headingRowHeight: 48,
+        headingRowColor: const WidgetStatePropertyAll(Colors.lightBlue),
+        fixedColumnsColor: Colors.lightGreen,
+        isHorizontalScrollBarVisible: true,
+        minWidth: 700,
       ),
     );
   }
