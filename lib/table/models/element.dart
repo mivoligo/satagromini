8b3f8e629f19d510560325c60a82b0ad4a9817c5
Michal Predotka <mivoligo@gmail.com>
2024-12-19 20:01:05 +0000

    Rework data for elements

---
+++ lib/table/models/element.dart
@@ -0,0 +1,23 @@
+class Element {
+  const Element({
+    required this.name,
+    required this.atomicNumber,
+    required this.weight,
+    required this.density,
+    required this.meltingPoint,
+    required this.boilingPoint,
+    required this.heatCapacity,
+    required this.electroNegativity,
+    required this.abundance,
+  });
+
+  final String name;
+  final double atomicNumber;
+  final double weight;
+  final double density;
+  final double meltingPoint;
+  final double boilingPoint;
+  final double heatCapacity;
+  final double electroNegativity;
+  final double abundance;
+}
--- lib/table/views/table_page.dart
+++ lib/table/views/table_page.dart
@@ -2,8 +2,28 @@ import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:satagromini/table/data/elements.dart';
 
-class TablePage extends StatelessWidget {
+class TablePage extends StatefulWidget {
   const TablePage({super.key});
+
+  @override
+  State<TablePage> createState() => _TablePageState();
+}
+
+class _TablePageState extends State<TablePage> {
+  int? sortColumnIndex;
+  bool sortAscending = true;
+
+  // void _sort<T>(
+  //     Comparable<T> Function(Dessert d) getField,
+  //     int columnIndex,
+  //     bool ascending,
+  //     ) {
+  //   _dessertsDataSource.sort<T>(getField, ascending);
+  //   setState(() {
+  //     sortColumnIndex = columnIndex;
+  //     sortAscending = ascending;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +34,29 @@ class TablePage extends StatelessWidget {
       body: DataTable2(
         columns: [
           const DataColumn2(label: Text('Element'), size: ColumnSize.M),
-          ...elementsData.values.first.indexed.map(
+          ...elementsData.indexed.map(
             (indexedList) => DataColumn2(
               label: Text('${indexedList.$1 + 1}'),
               numeric: true,
               onSort: (columnIndex, ascending) {},
             ),
+          ),
+        ],
+        rows: [
+          ...elementsData.map(
+            (element) => DataRow2(cells: [
+              DataCell(Text(element.name)),
+              DataCell(Text(element.atomicNumber.toString())),
+              DataCell(Text(element.weight.toString())),
+              DataCell(Text(element.density.toString())),
+              DataCell(Text(element.meltingPoint.toString())),
+              DataCell(Text(element.boilingPoint.toString())),
+              DataCell(Text(element.heatCapacity.toString())),
+              DataCell(Text(element.electroNegativity.toString())),
+              DataCell(Text(element.abundance.toString())),
+            ]),
           )
         ],
-        rows: elementsData.entries
-            .map(
-              (entry) => DataRow2(
-                cells: [
-                  DataCell(Text(entry.key)),
-                  ...entry.value.map(
-                    (doubleValue) => DataCell(Text(doubleValue.toString())),
-                  )
-                ],
-              ),
-            )
-            .toList(growable: false),
         columnSpacing: 12,
         fixedTopRows: 1,
         fixedLeftColumns: 1,
