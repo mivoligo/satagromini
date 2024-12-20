e9d27ea2d2b5a2c4fb59e56ed4f61bace3776896
Michal Predotka <mivoligo@gmail.com>
2024-12-20 10:36:04 +0000

    Add flutter_map and experiment with polygons

--- lib/poligons/views/map_page.dart
+++ lib/poligons/views/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -8,6 +10,50 @@ class MapPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter:
+              LatLng(51.509364, -0.128928), // Center the map over London
+          initialZoom: 9.2,
+          onTap: (tapPosition, point) {
+            print(tapPosition.global);
+            print(point);
+          },
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+          PolygonLayer(
+            polygons: [
+              Polygon(
+                points: [
+                  LatLng(0, 0),
+                  LatLng(0, 5),
+                  LatLng(2, 4),
+                  LatLng(-2, -2)
+                ],
+                color: Colors.black26,
+                borderColor: Colors.red,
+                borderStrokeWidth: 2,
+                label: 'LABEL W',
+              ),
+            ],
+          ),
+          MarkerLayer(markers: [
+            Marker(
+                point: LatLng(0, 1),
+                child: SizedBox(
+                  width: 10,
+                  height: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.blue),
+                  ),
+                ))
+          ]),
+        ],
       ),
     );
   }
