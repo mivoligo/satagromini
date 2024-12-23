4554d85ebc77fed8b65bfd9beddb7ddb8d666fe7
Michal Predotka <mivoligo@gmail.com>
2024-12-23 10:49:42 +0000

    Allow legend scroll on small screens

--- lib/table/views/widgets/legend.dart
+++ lib/table/views/widgets/legend.dart
@@ -8,17 +8,19 @@ class Legend extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          SingleLegend(name: '1', description: 'Atomic number'),
-          SingleLegend(name: '2', description: 'Standard atomic weight'),
-          SingleLegend(name: '3', description: 'Density'),
-          SingleLegend(name: '4', description: 'Melting point'),
-          SingleLegend(name: '5', description: 'Boiling point'),
-          SingleLegend(name: '6', description: 'Specific heat capacity'),
-          SingleLegend(name: '7', description: 'Electro-negativity'),
-          SingleLegend(name: '8', description: 'Abundance in Earth\'s crust'),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleLegend(name: '1', description: 'Atomic number'),
+            SingleLegend(name: '2', description: 'Standard atomic weight'),
+            SingleLegend(name: '3', description: 'Density'),
+            SingleLegend(name: '4', description: 'Melting point'),
+            SingleLegend(name: '5', description: 'Boiling point'),
+            SingleLegend(name: '6', description: 'Specific heat capacity'),
+            SingleLegend(name: '7', description: 'Electro-negativity'),
+            SingleLegend(name: '8', description: 'Abundance in Earth\'s crust'),
+          ],
+        ),
       ),
     );
   }
